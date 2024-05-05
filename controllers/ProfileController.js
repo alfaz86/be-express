a31b43f9a89777e0a56087cbf40dbad0ff9a2478
alfaz86 <hidecode403@gmail.com>
2024-05-05 15:02:09 +0700

    setup prisma

--- controllers/ProfileController.js
+++ controllers/ProfileController.js
@@ -1,8 +1,11 @@
+const { PrismaClient } = require("@prisma/client");
 const response = require("./../lib/response")
+
+const prisma = new PrismaClient();
 
 exports.getProfile = async (req, res) => {
     try {
-        const data = [];
+        const data = await prisma.profile.findMany();
         response(200, data, 'Get All Data', res);
     } catch (error) {
         response(500, error, 'Data Not Found', res);
