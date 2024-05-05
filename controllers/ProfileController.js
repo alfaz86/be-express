8b902fc08d3f3ef95fc76a1c49b6c372c577191f
alfaz86 <hidecode403@gmail.com>
2024-05-05 16:48:25 +0700

    log prisma db

--- controllers/ProfileController.js
+++ controllers/ProfileController.js
@@ -4,6 +4,7 @@ const response = require("./../lib/response")
 exports.getProfile = async (req, res) => {
     try {
         const data = await prisma.profile.findMany();
+        console.log(data);
         response(200, data, 'Get All Data', res);
     } catch (error) {
         response(500, error, 'Data Not Found', res);
