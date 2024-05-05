0a0a951194c67b4cfd2a58c5cf2f5fc3182c6ce5
alfaz86 <hidecode403@gmail.com>
2024-05-05 16:06:40 +0700

    show tables

--- api/index.js
+++ api/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const dotenv = require('dotenv').config();
 const ProfileRoute = require("./../routes/ProfileRoute.js");
+const getAllTableNames = require("../lib/database.js");
 
 const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/db", getAllTableNames);
 app.use(ProfileRoute);
 
 app.listen(port, () => console.log(`The server runs on... http://localhost:${port}`));
