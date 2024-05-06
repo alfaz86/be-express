606b1e34074aa1077564005f53d64141058b6cda
alfaz86 <hidecode403@gmail.com>
2024-05-06 16:18:47 +0700

    setting env

--- api/index.js
+++ api/index.js
@@ -2,9 +2,8 @@ const express = require("express");
 const app = express();
 const dotenv = require('dotenv').config();
 const ProfileRoute = require("./../routes/ProfileRoute.js");
-const getAllTableNames = require("../lib/database.js");
 
-const port = process.env.PORT || 8080;
+const port = process.env.APP_PORT || 8080;
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 app.use(ProfileRoute);
